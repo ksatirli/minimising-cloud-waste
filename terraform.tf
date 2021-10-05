bcb4d1a3605467229cafdec8ee59ccb35cab80e9
Kerim Satirli <k.satirli@gmail.com>
2021-10-05 12:35:51 +0200

    adds Terraform Cloud support

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,14 @@
 terraform {
+  # see https://www.terraform.io/docs/language/settings/backends/remote.html
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "ksatirli"
+
+    workspaces {
+      name = "minimize-cloud-waste"
+    }
+  }
+
   # see https://www.terraform.io/docs/language/settings/index.html#specifying-a-required-terraform-version
   required_version = ">= 1.0.8"
 }
